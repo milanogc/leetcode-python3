b9e18d17a91617f9de43d6ab8804f4ddf30c5796
Milano Gadelha Carvalho <milanogc@gmail.com>
2022-02-23 14:26:57 -0300

    N-Queens

---
+++ n-queens.py
@@ -0,0 +1,45 @@
+# https://leetcode.com/problems/n-queens/
+
+from typing import List
+
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        ans = []
+        board = [["."] * n for _ in range(n)]
+        cols = set()
+        pos_diags = set()
+        neg_diags = set()
+
+        def recursive(row: int):
+            if row == n:
+                ans.append(["".join(board[i]) for i in range(n)])
+
+            for col in range(n):
+                pos_diag = row + col
+                neg_diag = row - col
+
+                if (
+                    col not in cols
+                    and pos_diag not in pos_diags
+                    and neg_diag not in neg_diags
+                ):
+                    board[row][col] = "Q"
+                    cols.add(col)
+                    pos_diags.add(pos_diag)
+                    neg_diags.add(neg_diag)
+                    recursive(row + 1)
+                    # backtrack
+                    board[row][col] = "."
+                    cols.remove(col)
+                    pos_diags.remove(pos_diag)
+                    neg_diags.remove(neg_diag)
+
+        recursive(0)
+        return ans
+
+
+assert Solution().solveNQueens(4) == [
+    [".Q..", "...Q", "Q...", "..Q."],
+    ["..Q.", "Q...", "...Q", ".Q.."],
+]
